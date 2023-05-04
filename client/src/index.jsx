6c9384bff24518570a1e6f1ff030788dbe6fac27
ZacharyFeltman <zachary.feltman@gmail.com>
2023-05-04 15:37:37 -0600

    working on Slice features

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//!!Check if react redux install worked!!
+import {Provider} from "react-redux";
+import store from './redux/store';
 
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
@@ -11,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //edited out react.strictmode tags as well. kept App
     //<React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
     //</React.StrictMode>
 );
 
